54b77d7d20d87e3f6eceb6ab5138c0a759074db1
Das Pups <178408@gmail.com>
2016-01-08 18:33:18 +0400

    It works!

--- bash/exam/homework.sh
+++ bash/exam/homework.sh
@@ -16,79 +16,8 @@ quit(){
  continue 
 }
 
-#echo "Watch processes list"
-#ps ux
-
-while [ true ];
-do 
- 
- echo
- echo "Please, choose option:"
- echo "(1) Display all processes"
- echo "(2) Display processes by name"
- echo "(3) Input PID(s) for sending signals"
- echo "(4) Quit "
-
- #echo "Input PIDs of processes"
- read UserAnsw
- 
- case "$UserAnsw" in
-  1) ps -xco pid,uid,tty,stat,cmd
-	 GROUP=-1
-	 continue  
-     ;;
-  2) echo "Input name of process"
- 	 read PNAME
-	 GROUP=$(pidof -x $PNAME)
-
-	 if [ $? != 0 ]; then
-	  echo
-	  echo "Process with name $PNAME not found" >&2
-	  continue	  
-	 fi 
-
-	 ps -co pid,uid,tty,stat,cmd -p $GROUP
-	 continue
-     ;;
-  3) if [ "$GROUP" == "-1" ]; then
-	   echo "Please, input PID(s):"
-	 else
-	   echo "Please, input PID(s) (press 0 to select all):"
-	 fi
-	  
-	 read PID
-
-	 # group of PIDs with same name
-	 if [ "$GROUP" != "-1" ] && [ "$PID" == "0" ]; then
-	   PID=$(echo $GROUP) 
-	   echo "All"
-	   echo "$PID"      
-	 else
-       re='^[1-9][0-9]*(\s[1-9][0-9]*)*$'
- 
-       if ! [[ $PID =~ $re ]] ; then
-		echo "Wrong PID(s)" >&2
-		quit 250
-	   fi 
-
-	   # verify PIDs are exsist
-	   PIDS=$(echo $PID)
-	   stop=0
-	   for p in $PIDS; do
-         ps -p $p > /dev/null
-         
-	     if [ $? != 0 ]; then
-	      echo "PID $p not found" >&2
-	      stop=1
-	      break 
-	     fi
-	   done
-	   
-	   if [ $stop == 1 ]; then 
-	    quit 250
-	   fi	   
-	 fi
-   while [ true];
+SendSignals(){
+  while [ true ];
    do
     echo ""
     echo "Input number of signal:"
@@ -100,31 +29,34 @@ do
     echo "20 for SIGTSTP" # stop like Ctrl-Z
     echo "18 for SIGCONT" # continue
     echo "Another number of signal that you want"
+    echo "0  for return"
 
     read SIG
     case $SIG in
+	 0) break
+		;;
      1) echo "Sending signal 1"
-       kill -1 $UserAnsw
+       kill -1 $PIDS
        ;; 
      2) echo "Sending signal 2"
-       kill -2 $UserAnsw
+       kill -2 $PIDS
        ;;
      9) echo "Sending signal 9"
-       kill -9 $UserAnsw
+       kill -9 $PIDS
        ;; 
      15) echo "Sending signal 15"
-       kill -15 $UserAnsw  
+       kill -15 $PIDS  
        ;;
      18) echo "Sending signal 18"
-       kill -18 $UserAnsw
+       kill -18 $PIDS
        ;; 
      20) echo "Sending signal 20"
-       kill -20 $UserAnsw 
+       kill -20 $PIDS 
        ;;
      $SIG)  
 	   if (($SIG>0 && $SIG<65)); then
         echo "Sending signal $SIG" 	
-        kill -$SIG $UserAnsw
+        kill -$SIG $PIDS
        else
         echo "Wrong number of signal! Input number from 1 to 64." >&2
         quit 250
@@ -132,6 +64,79 @@ do
        ;;
     esac
    done
+}
+
+#echo "Watch processes list"
+#ps ux
+
+while [ true ];
+do 
+ 
+ echo
+ echo "Please, choose option:"
+ echo "(1) Display all processes"
+ echo "(2) Send signal by name"
+ echo "(3) Send signal by PID(s)"
+ echo "(4) Quit "
+
+ read OPT
+ 
+ case "$OPT" in
+  1) ps -xco pid,uid,tty,stat,cmd
+
+	 continue  
+     ;;
+  2) echo "Please, input name of process"
+
+ 	 read PNAME
+
+	 PIDS=$(pidof -x $PNAME)
+
+	 if [ $? != 0 ]; then
+	  echo
+	  echo "Process with name $PNAME not found" >&2
+	  continue	  
+	 fi 
+
+	 ps -co pid,uid,tty,stat,cmd -p $PIDS
+
+     SendSignals
+
+	 continue
+     ;;
+  3) echo "Please, input PID(s)"
+	  
+	 read PIDS
+
+     # verify that PIDs are correct
+     re='^[1-9][0-9]*(\s[1-9][0-9]*)*$'
+ 
+     if ! [[ $PIDS =~ $re ]] ; then
+	   echo "Incorrect PID(s)" >&2
+	   quit 250
+	 fi 
+
+	 # verify that processes are exsist
+	 PIDS=$(echo $PIDS)
+	 stop=0
+	 for PID in $PIDS; do
+       ps -p $PID > /dev/null
+       
+	   if [ $? != 0 ]; then
+	    echo "PID $PID not found" >&2
+	    stop=1
+	    break 
+	   fi
+	 done
+	   
+	 if [ $stop == 1 ]; then 
+	  quit 250
+	 fi
+   
+     SendSignals
+
+	 continue
+     ;;
   4) exit 0   
      ;;
   *) echo "Wrong option!" >&2
